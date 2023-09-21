98deb924bafccaae98addcacbd7140576c24f41b
baichangda <471267877@qq.com>
2023-09-21 16:09:25 +0800

    Changes

--- util/os.go
+++ util/os.go
@@ -3,35 +3,20 @@ package util
 import (
 	"os"
 	"os/signal"
-	"sync"
 )
 
-var funcs []func() error
-var funcsLock = sync.Mutex{}
-
 func ExitOnKill(fnOnExit func() error) {
-	funcsLock.Lock()
-	defer funcsLock.Unlock()
-	funcs = append(funcs, fnOnExit)
-	if len(funcs) == 1 {
-		go func() {
-			c := make(chan os.Signal)
-			defer close(c)
-			signal.Notify(c, os.Interrupt, os.Kill)
-			for v := range c {
-				Log.Infof("exit signal %+v", v)
-				funcsLock.Lock()
-				for _, f := range funcs {
-					if f != nil {
-						err := f()
-						if err != nil {
-							Log.Errorf("%+v", err)
-						}
-					}
-				}
-				funcsLock.Unlock()
-				os.Exit(0)
+	go func() {
+		c := make(chan os.Signal)
+		defer close(c)
+		signal.Notify(c, os.Interrupt, os.Kill)
+		s := <-c
+		Log.Infof("exit signal %+v", s)
+		if fnOnExit != nil {
+			err := fnOnExit()
+			if err != nil {
+				Log.Errorf("%+v", err)
 			}
-		}()
-	}
+		}
+	}()
 }
