73bb9e9ae96e3d0b038dc1b030d2477513354d6b
baichangda <471267877@qq.com>
2024-01-15 18:05:08 +0800

    Changes

--- cmd_kafka/web/main.go
+++ cmd_kafka/web/main.go
@@ -5,12 +5,16 @@ import (
 	"context"
 	"embed"
 	"encoding/json"
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 	"github.com/segmentio/kafka-go"
 	"github.com/spf13/cobra"
 	"io"
-	"io/fs"
+	"net"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -32,20 +36,47 @@ func Cmd() *cobra.Command {
 		Use:   "web",
 		Short: "启动web服务器、可以发送数据到kafka",
 		Run: func(cmd *cobra.Command, args []string) {
-			gin.SetMode(gin.ReleaseMode)
+			//gin.SetMode(gin.ReleaseMode)
 			engine := gin.New()
 
-			sub, err2 := fs.Sub(FS, "resource")
-			if err2 != nil {
-				util.Log.Errorf("%+v", err2)
-				return
-			}
-			engine.StaticFS("/resource", http.FS(sub))
+			//sub, err2 := fs.Sub(FS, "resource")
+			//if err2 != nil {
+			//	util.Log.Errorf("%+v", err2)
+			//	return
+			//}
+			//engine.StaticFS("/resource", http.FS(sub))
 
-			//engine.Static("/resource", "cmd_kafka/web/resource")
+			engine.Static("/resource", "cmd_kafka/web/resource")
 
 			engine.GET("/", func(c *gin.Context) {
 				c.Redirect(http.StatusMovedPermanently, "/resource/producer.html")
+			})
+
+			engine.GET("/consumer", func(ctx *gin.Context) {
+				//升级websocket
+				wsConn, _, _, err := ws.UpgradeHTTP(ctx.Request, ctx.Writer)
+				if err != nil {
+					util.Log.Errorf("%+v", err)
+					return
+				}
+				go func() {
+					defer wsConn.Close()
+					//获取参数
+					wsClient := WsClient{}
+
+					var kafkaAddrs = strings.Split(ctx.Query("kafkaAddrs"), ",")
+					var kafkaTopic = ctx.Query("kafkaTopic")
+					var kafkaGroupId = ctx.Query("kafkaGroupId")
+
+					//初始化ws客户端对象
+					err = wsClient.init(kafkaAddrs, kafkaTopic, kafkaGroupId, wsConn, wsClient.onWsMsg)
+					if err != nil {
+						util.Log.Errorf("%+v", err)
+						return
+					}
+
+					util.Log.Infof("-----exit ws-----")
+				}()
 			})
 
 			engine.POST("/producer", func(ctx *gin.Context) {
@@ -107,4 +138,159 @@ func Cmd() *cobra.Command {
 	}
 	cmd.Flags().IntVarP(&port, "port", "p", 23456, "web服务端口")
 	return &cmd
+}
+
+type WsClient struct {
+	conn    net.Conn
+	stopCtx context.Context
+	stopFn  context.CancelFunc
+}
+
+func (e *WsClient) init(kafkaAddrs []string, kafkaTopic string, kafkaGroupId string, conn net.Conn, onMsg func(msg *InMsg)) error {
+	stopCtx, stopFn := context.WithCancel(context.Background())
+	e.conn = conn
+	e.stopCtx = stopCtx
+	e.stopFn = stopFn
+
+	go func() {
+		reader := kafka.NewReader(kafka.ReaderConfig{
+			Brokers: kafkaAddrs,
+			GroupID: kafkaGroupId,
+			Topic:   kafkaTopic,
+			Dialer: &kafka.Dialer{
+				Timeout:   3 * time.Second,
+				DualStack: true,
+			},
+		})
+		defer reader.Close()
+
+		//go func() {
+		//	for {
+		//		select {
+		//		case <-stopCtx.Done():
+		//			return
+		//		case <-time.After(1 * time.Second):
+		//			stats := reader.Stats()
+		//			util.Log.Infof("%+v", stats)
+		//		}
+		//	}
+		//}()
+		//
+		//e.send(&OutMsg{
+		//	Flag:    102,
+		//	Data:    "",
+		//	Succeed: true,
+		//})
+
+		util.Log.Infof("init kafkaAddrs[%s] kafkaTopic[%s] kafkaGroupId[%s]", strings.Join(kafkaAddrs, ","), kafkaTopic, kafkaGroupId)
+
+		for {
+			message, err := reader.ReadMessage(e.stopCtx)
+			if err != nil {
+				if errors.Is(err, context.Canceled) {
+					return
+				}
+				util.Log.Errorf("%+v", err)
+				e.send(&OutMsg{
+					Flag:    102,
+					Data:    err.Error(),
+					Succeed: false,
+				})
+				e.stopFn()
+				return
+			}
+			e.send(&OutMsg{
+				Flag:    101,
+				Data:    string(message.Value),
+				Succeed: true,
+			})
+		}
+	}()
+
+	//启动协程监听ws信息
+	for {
+		select {
+		case <-stopCtx.Done():
+			return nil
+		default:
+			err := conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
+			if err != nil {
+				util.Log.Errorf("%+v", err)
+				return nil
+			}
+			text, err := wsutil.ReadClientText(conn)
+			if err != nil {
+				if os.IsTimeout(err) {
+					continue
+				} else {
+					util.Log.Errorf("%+v", err)
+					return nil
+				}
+			}
+			inMsg := InMsg{}
+			err = json.Unmarshal(text, &inMsg)
+			if err != nil {
+				util.Log.Errorf("%+v", err)
+				continue
+			}
+			onMsg(&inMsg)
+		}
+	}
+
+}
+
+func (e *WsClient) send(msg *OutMsg) {
+	marshal, err := json.Marshal(msg)
+	if err != nil {
+		e.stopFn()
+		util.Log.Errorf("%+v", err)
+	}
+	err = wsutil.WriteServerText(e.conn, marshal)
+	if err != nil {
+		e.stopFn()
+		util.Log.Errorf("%+v", err)
+	}
+}
+
+type InMsg struct {
+	/**
+	1、连接kafka开始消费
+	*/
+	Flag int    `json:"flag"`
+	Data string `json:"data"`
+}
+
+type OutMsg struct {
+	/**
+	1、连接kafka开始消费
+	101、接收到kafka数据
+	102、kafka断开通知
+	*/
+	Flag    int    `json:"flag"`
+	Data    string `json:"data"`
+	Succeed bool   `json:"succeed"`
+}
+
+type KafkaConfig struct {
+	kafkaAddrs   []string
+	kafkaTopic   string
+	kafkaGroupId string
+}
+
+func (e *WsClient) onWsMsg(msg *InMsg) {
+	switch msg.Flag {
+	default:
+		util.Log.Warnf("flag[%d] not support", msg.Flag)
+	}
+}
+
+func (e *WsClient) HandleConsume(data string) {
+	kafkaConfig := KafkaConfig{}
+	err := json.Unmarshal([]byte(data), &kafkaConfig)
+	if err != nil {
+		util.Log.Errorf("%+v", e)
+		e.stopFn()
+		return
+	}
+
 }
