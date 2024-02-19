57f1a1cb13bedde205bf505b34c9207dbb245334
baichangda <471267877@qq.com>
2024-02-19 13:59:37 +0800

    Changes

--- cmd_pressTest/main.go
+++ cmd_pressTest/main.go
@@ -2,6 +2,7 @@ package cmd_pressTest
 
 import (
 	"bcd-util/cmd_pressTest/gb32960"
+	"bcd-util/cmd_pressTest/immotors"
 	"github.com/spf13/cobra"
 )
 
@@ -11,5 +12,6 @@ func Cmd() *cobra.Command {
 		Short: "压力测试",
 	}
 	cmd.AddCommand(gb32960.Cmd())
+	cmd.AddCommand(immotors.Cmd())
 	return &cmd
 }
