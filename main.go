bd149511360dfc17a3bfc1290ebc13dd1e106b43
baichangda <471267877@qq.com>
2024-03-20 14:03:41 +0800

    Changes

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"bcd-util/cmd_kafka"
 	"bcd-util/cmd_simlator"
-	"bcd-util/util"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{}
 
 func main() {
-	util.StartWeb_pprof()
+	//util.StartWeb_pprof()
 	//rootCmd.AddCommand(cmd_redis.Cmd())
-	//rootCmd.AddCommand(cmd_kafka.Cmd())
+	rootCmd.AddCommand(cmd_kafka.Cmd())
 	//rootCmd.AddCommand(cmd_monitor.Cmd())
 	//rootCmd.AddCommand(cmd_pressTest.Cmd())
 	rootCmd.AddCommand(cmd_simlator.Cmd())
