6a7411c6a7127368bbe77c66dc3a9e8ec1b0d541
baichangda <471267877@qq.com>
2024-05-31 15:40:14 +0800

    Changes

--- util/pprof.go
+++ util/pprof.go
@@ -2,7 +2,7 @@ package util
 
 import (
 	"net/http"
-	"net/http/pprof"
+	_ "net/http/pprof"
 )
 
 // StartWeb_pprof 开启pprof性能采集web服务、监听在54321端口
@@ -10,14 +10,8 @@ import (
 // 采集内存信息到文件 curl -o mem.out http://127.0.0.1:54321/debug/pprof/allocs?seconds=30
 func StartWeb_pprof() {
 	go func() {
-		mux := http.NewServeMux()
-		mux.HandleFunc("/debug/pprof/", pprof.Index)
-		mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 		Log.Infof("start pprof web ListenAndServe[:54321]")
-		err := http.ListenAndServe(":54321", mux)
+		err := http.ListenAndServe(":54321", nil)
 		if err != nil {
 			Log.Errorf("%+v", err)
 			return
